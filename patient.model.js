0234914e4e0a4a8d47ff0635fa62da5908de5ad8
AnuragTheCoder <129210052+AnuragTheCoder@users.noreply.github.com>
2023-12-20 22:19:21 +0530

    completed Patient

--- patient.model.js
+++ patient.model.js
@@ -2,6 +2,52 @@ import mongoose from "mongoose"
 
 const PatientSchema=new mongoose.Schema({
 
+name:{
+    type:String,
+    required:true,
+},
+image:{
+    type:String,
+},
+
+diagnosedWith:{
+    type:String,
+    required:true,
+},
+
+address:{
+    type:String,
+    required:true,
+},
+
+age:{
+    type:Number,
+    required:true,
+},
+
+BloodGroup:{
+    enum:["AB+","A+","B+","O+","AB-","A-","B-","O-"],
+    type:String,
+    required:true,
+},
+
+gender:{
+    type:String,
+    enum:["M","F","O"],
+    required:true,
+},
+
+admittedIn:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Hospital"
+
+},
+
+doctorReferred:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Doctor"
+}
+
 
 
 
